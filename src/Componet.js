d09e7dab9e65db648536f4b18b4f18ea2bd96c93
CamiloByte <92604258+CamiloByte@users.noreply.github.com>
2024-05-15 21:01:12 -0500

    hook en react

--- src/Componet.js
+++ src/Componet.js
@@ -1,7 +1,23 @@
-import React from 'react' 
+import React, { useState } from 'react' 
+
 
 export default function Componet() {
+
+    const [text, setText] = useState()
+    const [updated, setupdated] = useState()
+
+    
+    const textOnChange = (event) => {setText(event.target.value)}
+    const buttonOnClick = (event) => {setupdated(text)}
+
   return (
-    <div>My Componet</div>
+    <div>
+        <input type="text" value = {text} onChange={textOnChange} />
+        <button onClick={buttonOnClick}>  Actualizar</button>
+        <p>Texto input: {text}</p>
+        <p>Texto Actualizado: {updated} </p>
+
+
+    </div>
   )
 }
